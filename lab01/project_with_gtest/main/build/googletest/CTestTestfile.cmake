c106704394b12c2b4801d85d7ff9e8285a3a80b5
VladiM1R41 <vova_layhov@mail.ru>
2023-09-22 19:59:18 +0300

    adding modules

---
+++ lab01/project_with_gtest/main/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/code/test/project_with_gtest/main/googletest
+# Build directory: C:/code/test/project_with_gtest/main/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ lab01/project_with_gtest/main/build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/code/test/project_with_gtest/main/googletest/googletest
+# Build directory: C:/code/test/project_with_gtest/main/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
