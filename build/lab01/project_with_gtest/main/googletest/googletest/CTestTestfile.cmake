bfea5eb672e43842845a9ce34454903ae0bcc0f9
VladiM1R41 <vova_layhov@mail.ru>
2023-09-22 20:14:19 +0300

    s

---
+++ build/lab01/project_with_gtest/main/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/code/test01/OOP/lab01/project_with_gtest/main/googletest/googletest
+# Build directory: C:/code/test01/OOP/build/lab01/project_with_gtest/main/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ lab01/project_with_gtest/main/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+int main()
+{
+  int num;
+  cout << "num";
+  cin  >> num;
+
+  cout << "result: " << calculator(num) << endl;
+
+  return 0;
+}
